6d99e6e2af6e6869d9ea9ab869c8f415f616dc44
Pooja B Naik <53901523+Pooja710-naik@users.noreply.github.com>
2021-03-22 21:27:02 +0530

    Update face expression.py

--- face expression.py
+++ face expression.py
@@ -1,6 +1,5 @@
 import cv2
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 def detect(gray, frame): 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
